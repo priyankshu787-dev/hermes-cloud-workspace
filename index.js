9a0fb456835bad57bffcace11c160d48f37c650b
Bikash Bauri <priyankshu787@gmail.com>
2026-09-10 15:38:46 +0530

    Add charset=utf-8 to Content-Type header
    Updated Content-Type header to include charset=utf-8.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   res.end(`
     <div style="background:#0b0f19; color:white; font-family:sans-serif; text-align:center; padding-top:100px;">
       <h1 style="color:#f59e0b;">🛡️ Hermes Workspace Dashboard</h1>
